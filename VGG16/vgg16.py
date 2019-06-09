bd20bd4a3d4e5773ae4008dac710ac5c7b618a81
zhangx2019 <49628720+zhangx2019@users.noreply.github.com>
2019-06-09 23:46:19 +0800

    Update vgg16.py

--- VGG16/vgg16.py
+++ VGG16/vgg16.py
@@ -1,12 +1,4 @@
-########################################################################################
-# Davi Frossard, 2016                                                                  #
-# VGG16 implementation in TensorFlow                                                   #
-# Details:                                                                             #
-# http://www.cs.toronto.edu/~frossard/post/vgg16/                                      #
-#                                                                                      #
-# Model from https://gist.github.com/ksimonyan/211839e770f7b538e2d8#file-readme-md     #
-# Weights from Caffe converted using https://github.com/ethereon/caffe-tensorflow      #
-########################################################################################
+# -*- coding: utf-8 -*-                                                                                     
 
 import tensorflow as tf
 import numpy as np
